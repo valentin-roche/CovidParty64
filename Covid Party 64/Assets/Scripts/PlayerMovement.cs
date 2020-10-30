0d09c6b4a4f0460ec122ec22ddf1de1ae2c5dea6
H-Fox <hugorenard.31@hotmail.fr>
2020-10-30 17:54:21 +0100

    Movement and Jump Implementation

---
+++ Covid Party 64/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    public bool isJumping;
+    public bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask collisionLayers;
+
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+    private float horizontalMovement;
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
+
+        horizontalMovement = Input.GetAxis("Horizontal")*moveSpeed*Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded) 
+        {
+            isJumping = true;
+        }
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer(horizontalMovement);
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        if(isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
