96565d03849ae9a2f6a1018324b87da1ec8dcdfc
Valentin Roche <35733063+valentin-roche@users.noreply.github.com>
2020-11-02 17:01:29 +0100

    modification de repertoire des scripts joueur

---
+++ Covid Party 64/Assets/Scenes/PlayerFolder/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private bool isJumping;
+    private bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask collisionLayer;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    private Vector3 velocity = Vector3.zero;
+    private float horizontalMovement;
+
+    private bool isFacingRight = true;
+    
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayer);
+
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+
+        float characterVeclocity = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("Speed", characterVeclocity);
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer(horizontalMovement);
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        if(_horizontalMovement > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+        else if(_horizontalMovement < 0 && isFacingRight)
+        {
+            Flip();
+        }
+
+        if(isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
---
+++ Covid Party 64/Assets/Scenes/PlayerFolder/Shoot.cs
@@ -0,0 +1,60 @@
+﻿
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public Transform gun;
+
+    Vector2 direction;
+
+    public GameObject bullet;
+    public float bulletSpeed;
+
+    public Transform shootPoint;
+
+    public float fireRate;
+    float readyForNextShot;
+   
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if(Time.time > readyForNextShot)
+            {
+                readyForNextShot = Time.time * 1 / fireRate;
+                shootHorizontal();
+            }
+            
+        } else if (Input.GetMouseButton(1))
+        {
+            if (Time.time > readyForNextShot)
+            {
+                readyForNextShot = Time.time * 1 / fireRate;
+                shootVertical();
+            }
+        }
+    }
+
+    void shootHorizontal()
+    {
+        Debug.Log("Horizontal shot");
+        GameObject BulletIns = Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+        BulletIns.GetComponent<Rigidbody2D>().AddForce(BulletIns.transform.right * bulletSpeed);
+        Destroy(BulletIns, 0.5f);
+    }
+
+    void shootVertical()
+    {
+        Debug.Log("Vertical shot");
+        GameObject BulletIns = Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+        BulletIns.GetComponent<Rigidbody2D>().AddForce(BulletIns.transform.up * bulletSpeed);
+        Destroy(BulletIns, 0.5f);
+    }
+}
