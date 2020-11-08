73fec82dea97c52702d209009399628b81092f77
Valentin Roche <35733063+valentin-roche@users.noreply.github.com>
2020-11-08 19:49:45 +0100

    finished pause menu (ugly but functional)

--- Covid Party 64/Assets/Scenes/LevelFolder/PauseMenu.cs
+++ Covid Party 64/Assets/Scenes/LevelFolder/PauseMenu.cs
@@ -2,28 +2,43 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject PauseMenuUI;
     private bool isPaused = false;
+    public string mainMenuScene;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             isPaused = !isPaused;
+            if (isPaused)
+            {
+                PauseMenuUI.SetActive(true);
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                PauseMenuUI.SetActive(false);
+                Time.timeScale = 1f;
+            }
         }
 
-        if (isPaused)
-        {
-            Debug.Log("Disp");
-            PauseMenuUI.SetActive(true);
-        }
-        else
-        {
-            Debug.Log("Hide");
-            PauseMenuUI.SetActive(true);
-        }
+        
+    }
+
+    public void resume()
+    {
+        isPaused = false;
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void exit()
+    {
+        SceneManager.LoadScene(mainMenuScene);
     }
 }
