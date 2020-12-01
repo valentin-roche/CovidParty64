c56b551d6c6daa767e91bc9447248ae92717a5ad
Seadwell <seadwell6@gmail.com>
2020-12-01 17:50:13 +0100

    Implémentation Objet + affichage

---
+++ Covid Party 64/Assets/PickUpObjet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PickUpObjet : MonoBehaviour
+{
+
+    void Start()
+    {
+        Physics2D.IgnoreLayerCollision(11, 13);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Inventory.instance.addObjet(gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
