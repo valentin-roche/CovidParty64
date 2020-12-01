4a1442f32b8f96ec97f6adf17fd500cd72e2cf2a
Valentin Roche <35733063+valentin-roche@users.noreply.github.com>
2020-12-01 15:41:13 +0100

    basic music manager

---
+++ Covid Party 64/Assets/LevelSoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSoundManager : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public AudioClip RegularMusic;
+    public AudioClip BossMusic;
+    public AudioClip IntroMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayRegular()
+    {
+        if (AudioSource.isPlaying)
+        {
+            AudioSource.Stop();
+        }
+        AudioSource.clip = RegularMusic;
+        AudioSource.Play();
+        AudioSource.loop = true;
+    }
+
+    public void PlayBoss()
+    {
+        if (AudioSource.isPlaying)
+        {
+            AudioSource.Stop();
+        }
+        AudioSource.clip = BossMusic;
+        AudioSource.Play();
+        AudioSource.loop = true;
+    }
+
+    public void PlayIntro()
+    {
+        if (AudioSource.isPlaying)
+        {
+            AudioSource.Stop();
+        }
+        AudioSource.clip = IntroMusic;
+        AudioSource.Play();
+        AudioSource.loop = true;
+    }
+}
