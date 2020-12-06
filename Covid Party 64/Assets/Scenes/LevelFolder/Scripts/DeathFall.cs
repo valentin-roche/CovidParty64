4c2b6d915e88c274712a8ae94e067ee360c7aba5
ArthurVigneault <arthur.vigneault@laposte.net>
2020-12-06 17:06:16 +0100

    8 niveaux fait + refactor
    Tilemaps + levels crée
    Refactor folder level et enemy

---
+++ Covid Party 64/Assets/Scenes/LevelFolder/Scripts/DeathFall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathFall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        switch (col.tag)
+        {
+            case "EnemyS":
+                col.gameObject.GetComponent<EnemySmallAI>().death();
+                break;
+            case "EnemyM":
+                col.gameObject.GetComponent<EnemyMedAI>().death();
+                break;
+            case "EnemyL":
+                col.gameObject.GetComponent<EnemyLargeAI>().death();
+                break;
+        }
+    }
+}
