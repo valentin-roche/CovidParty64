738208b9c3c664f6dae46ef6761b6e543afe60da
Valentin Roche <35733063+valentin-roche@users.noreply.github.com>
2020-12-06 15:36:12 +0100

    update boss ai according to enemy modifications
    also moved scripts and updated boss sound manager

--- Covid Party 64/Assets/Scenes/Boss/BossAI.cs
+++ Covid Party 64/Assets/Scenes/Boss/BossAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,38 +14,55 @@ public class BossAI : MonoBehaviour
     public SecondPeriodState secondPeriod = new SecondPeriodState();
     #endregion
 
+    // States variables
     private StateMachine.StateMachine stateMachine;
     private bool hasRevived = false;
+    
+    // Stats variables
     public int life;
-
-    public Transform target;
-
     public int speed;
+
+    // Audio variable
+    public BossSoundManager SoundManager;
+
+    // AI variables
+    public Transform target;
     public float nextWaypointDistance = 3f;
-
     public Transform enemyGFX;
-
+    private bool isGrounded;
+    private float groundCheckRadius;
     Pathfinding.Path path;
     int currentWaypoint = 0;
     bool reachedEndOfPath = false;
-
+    private LayerMask collisionLayer;
     Pathfinding.Seeker seeker;
     Rigidbody2D rb;
+    public Transform groundCheck;
+
+    // Animator variable
+    //public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        // States init
         normal = new NormalState();
         faster = new FasterState();
         stronger = new StrongerState();
         berzerk = new BerzerkState();
         secondPeriod = new SecondPeriodState();
+
+        // Stats init
         speed = Stats.BossStat.Speed;
-        target = GameObject.Find("Player").transform;
         life = Stats.BossStat.MaxHP;
 
+        // AI init
+        target = GameObject.Find("Player").transform;
         seeker = GetComponent<Pathfinding.Seeker>();
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); 
+        groundCheckRadius = 0.25f;
+        collisionLayer = LayerMask.GetMask("Foundation");
+        enemyGFX = this.transform;
 
         InvokeRepeating("UpdatePath", 0f, 0.5f);
 
@@ -62,13 +80,15 @@ public class BossAI : MonoBehaviour
         }
     }
 
+    //Mise à jour du chemin
     void UpdatePath()
     {
         if (seeker.IsDone())
             seeker.StartPath(rb.position, target.position, OnPathComplete);
     }
 
-    void OnPathComplete(Pathfinding.Path p)
+    //La fin du chemin est atteinte
+    void OnPathComplete(Path p)
     {
         if (!p.error)
         {
@@ -85,7 +105,10 @@ public class BossAI : MonoBehaviour
 
         if (life <= 0)
         {
-            Debug.Log("ded");
+            Debug.Log("Boss died");
+
+            // TODO Play death animation
+
             Destroy(gameObject);
         }
 
@@ -119,6 +142,17 @@ public class BossAI : MonoBehaviour
 
     void FixedUpdate()
     {
+        //Vérification de collision avec le sol
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayer);
+
+        /*if (isGrounded == true)
+            animator.SetBool("isJumping", false);
+        */
+        
+        //Empecher l'accéleration
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -speed, speed), Mathf.Clamp(rb.velocity.y, -speed, speed));
+
+        //Gestion de l'AI
         if (path == null)
             return;
 
@@ -144,6 +178,7 @@ public class BossAI : MonoBehaviour
             currentWaypoint++;
         }
 
+        //Changement d'orientation du sprite
         if (rb.velocity.x >= 0.01f)
         {
             enemyGFX.localScale = new Vector3(1f, 1f, 1f);
@@ -159,5 +194,29 @@ public class BossAI : MonoBehaviour
 
         Debug.Log("Remaining HP : " + life);
         life -= damage;
+    }
+
+    //Gestion des différentes collisions
+    // TODO unomment when boss animator is done
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        switch (col.tag)
+        {
+            case "Jump":
+                if (path.vectorPath[currentWaypoint].y < path.vectorPath[currentWaypoint + 1].y && isGrounded)
+                {
+                    rb.AddForce(Vector2.up * 300f);
+                    // animator.SetBool("isJumping", true);
+                }
+                break;
+
+            case "JumpHole":
+                if (path.vectorPath[currentWaypoint].y == path.vectorPath[currentWaypoint + 1].y && isGrounded)
+                {
+                    rb.AddForce(Vector2.up * 150f);
+                    // animator.SetBool("isJumping", true);
+                }
+                break;
+        }
     }
 }
